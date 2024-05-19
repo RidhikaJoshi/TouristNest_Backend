0b5acb74dd4dd8f95143924229536c470815fca8
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-05-19 16:41:08 +0530

    Update CORS configuration to allow requests from specific origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: "https://touristnest.onrender.com",
     credentials: true,
   })
 ); // this method is used to enable CORS with middleware options
